df1f9710eac07a41c13d5d767ee3fe34588d3b43
Sushant Kumar <sushantonly961@gmail.com>
2024-07-10 13:15:37 +0530

    backend done

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://mernyoutube:mernyoutube@cluster0.nj5g05u.mongodb.net/hospitals",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+  }
+};
+
+module.exports = connectDB;
+
+// mongodb+srv://mernyoutube:mernyoutube@cluster0.nj5g05u.mongodb.net/jasoberoi
